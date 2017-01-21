149072fcc455542bfa832bfa8e4c623fc4734f6f
Niole Nelson <niolenelson@gmail.com>
2017-01-21 13:48:23 -0800

    can get scores
    uses lambdas to improve sorting

--- hierarchicalClustering.py
+++ hierarchicalClustering.py
@@ -15,6 +15,8 @@ the numbers they point to are their score out of 5 for the reviewee
 
 want to cluster people based on their coworkers' opinions of them
 similar opinions, closer together
+
+need to make new data every time a new cluster is created
 '''
 
 
@@ -22,21 +24,28 @@ similar opinions, closer together
 def getPearsonCo(r1, r2):
     return numpy.corrcoef(r1, r2)
 
-def cmp(x, y):
-        if x[0] < y[0]:
-                return -1
-        if x[0] > y[0]:
-                return 1
-        return 0
+def cmp(x, y, accessor, reverse = False):
+        if reverse:
+            if accessor(x) > accessor(y):
+                    return -1
+            if accessor(x) < accessor(y):
+                    return 1
+            return 0
+        else:
+            if accessor(x) < accessor(y):
+                    return -1
+            if accessor(x) > accessor(y):
+                    return 1
+            return 0
 
 def getSharedElements(r1, r2):
-        n1 = map(lambda x: x[1], sorted([(k, v) for k, v in r1.items() if k in r2], lambda x, y: cmp(x, y)))
-        n2 = map(lambda x: x[1], sorted([(k, v) for k, v in r2.items() if k in r1], lambda x, y: cmp(x, y)))
+        n1 = map(lambda x: x[1], sorted([(k, v) for k, v in r1.items() if k in r2], lambda x, y: cmp(x, y, lambda a: a[0])))
+        n2 = map(lambda x: x[1], sorted([(k, v) for k, v in r2.items() if k in r1], lambda x, y: cmp(x, y, lambda a: a[0])))
         return [n1, n2]
 
-
 def getAllScores(reviews):
-        scores = collections.defaultdict(int)
+        scores = []
+
         for i in range(len(reviews)-1):
             for j in range(i+1, len(reviews)):
                 a = reviews[i]
@@ -44,10 +53,10 @@ def getAllScores(reviews):
                 for k, m in a.items():
                     for l, n in b.items():
                         shared = getSharedElements(m, n)
-                        scores[k+l] = getPearsonCo(shared[0], shared[1])[0, 1]
+                        score = getPearsonCo(shared[0], shared[1])[0, 1]
+                        scores.append((k, l, score))
 
-        return scores
-
+        return sorted(scores, lambda x, y: cmp(x, y, lambda a: a[2], True))
 
 
 class Cluster:
@@ -60,10 +69,9 @@ class Cluster:
         self.children = []
 
 with open('data.json') as data_file:
-    delta = .5
+    delta = .05
     data = json.load(data_file)
     res = getAllScores(data)
-
-    assert res["ab"] >= 0.995341 - delta and res["ab"] <= 0.995341 + delta
+    print res
 
     x = Cluster(data)
